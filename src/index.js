e59fb05fa6ce0a27283ca3c13640f5647e68fc70
Grenzen <grenzentur@rambler.ru>
2020-09-15 18:02:06 +0300

    feat: complite towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+    if (matrix === undefined) {
+        return [];
+    }
+    let newArr = [];
+    let counter = 1;
+    matrix.forEach((el) => {
+        if (Array.isArray(el)) {
+            
+            if (counter % 2 === 1) {
+                counter++;
+                newArr.push(el);
+            } else {
+                counter++;
+                newArr.push(el.reverse());
+            }
+        }
+    });
+    return newArr.flat();
 }
